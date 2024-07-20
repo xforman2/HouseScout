11b9ea61a8ac7aa07a99e42e9495afe6e478434b
Matúš Formánek <formanekmato@gmail.com>
2024-07-20 11:16:07 +0200

    feat: created appsettings config

--- Program.cs
+++ Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using HouseScout.model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+        var configuration = builder.Build();
+        
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                ConfigureServices(services, configuration);
+            })
+            .Build();
+        
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<HouseScoutContext>();
+                context.Estates.Add(new Estate());
+                Console.WriteLine(context.Estates.ToList());
+                context.SaveChanges();
+                
+                Console.WriteLine("Database context created successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+    }
+
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton(configuration);
+
+        services.AddDbContext<HouseScoutContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+    }
+}
