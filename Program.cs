30baf692da0a53eed51ec4618206b5ef682d9ee7
Matúš Formánek <formanekmato@gmail.com>
2024-08-24 13:11:15 +0200

    feat: added scope so dbcontext can run

--- Program.cs
+++ Program.cs
@@ -3,27 +3,26 @@ using Discord.WebSocket;
 using Discord.Interactions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-
+using Microsoft.Extensions.Hosting;
 using System.Reflection;
-
+using HouseScout.Clients;
+using HouseScout.Mappers;
+using HouseScout.Model;
 using Microsoft.EntityFrameworkCore;
 
 class Program
 {
     private static DiscordSocketClient _client;
     private static InteractionService _interactionService;
-    private static IServiceProvider _services;
-    private static IConfiguration _configuration;
 
     static async Task Main(string[] args)
     {
-        // Load the configuration
-        _configuration = BuildConfig();
+        // Build the host
+        using var host = CreateHostBuilder().Build();
 
-        _services = ConfigureServices();
-
-        _client = _services.GetRequiredService<DiscordSocketClient>();
-        _interactionService = _services.GetRequiredService<InteractionService>();
+        // Configure Discord client and interaction service
+        _client = host.Services.GetRequiredService<DiscordSocketClient>();
+        _interactionService = host.Services.GetRequiredService<InteractionService>();
 
         _client.Log += LogAsync;
         _interactionService.Log += LogAsync;
@@ -32,46 +31,68 @@ class Program
         _client.InteractionCreated += async interaction =>
         {
             var context = new SocketInteractionContext(_client, interaction);
-            await _interactionService.ExecuteCommandAsync(context, _services);
+            await _interactionService.ExecuteCommandAsync(context, host.Services);
         };
 
         // Initialize the client
-        string? token = _configuration["DiscordToken"];
+        var configuration = host.Services.GetRequiredService<IConfiguration>();
+        string? token = configuration["DiscordToken"];
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
 
         // Register commands
         _client.Ready += async () =>
         {
-            await _interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            await _interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), host.Services);
             await _interactionService.RegisterCommandsGloballyAsync();
         };
+        
+        // Main scope of application
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var dbContext = services.GetRequiredService<HouseScoutContext>();
+
+            var bezrealitkyClient = services.GetRequiredService<BezrealitkyGraphQLClient>();
+            var srealityClient = services.GetRequiredService<SrealityHttpClient>();
+
+            var bezrealitkyMapper = services.GetRequiredService<BezrealitkyMapper>();
+            var srealityMapper = services.GetRequiredService<SrealityMapper>();
+
+            var bezrealitkyResponse = await bezrealitkyClient.GetAdvertsAsync();
+            var srealityResponse = await srealityClient.GetSrealityData();
+
+            var bezrealitkyMappedData = bezrealitkyMapper.MapResponseToModel(bezrealitkyResponse);
+            var srealityMappedData = srealityMapper.MapResponseToModel(srealityResponse);
+
+            await dbContext.Estates.AddRangeAsync(bezrealitkyMappedData);
+            await dbContext.Estates.AddRangeAsync(srealityMappedData);
+
+            await dbContext.SaveChangesAsync();
+        }
 
         await Task.Delay(-1); // Keep the bot running
     }
 
-    private static IServiceProvider ConfigureServices()
-    {
-        return new ServiceCollection()
-            .AddSingleton(_configuration)
-            .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+    private static IHostBuilder CreateHostBuilder() =>
+        Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
             {
-                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
-            }))
-            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-            .AddDbContext<DbContext>(options =>
-                options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection")))
-            .BuildServiceProvider();
-    }
+                var configuration = context.Configuration;
 
-    private static IConfiguration BuildConfig()
-    {
-        var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-        return builder.Build();
-    }
+                services.AddSingleton(configuration)
+                        .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                        {
+                            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+                        }))
+                        .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                        .AddDbContext<HouseScoutContext>(options =>
+                            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")))
+                        .AddSingleton<BezrealitkyGraphQLClient>()
+                        .AddSingleton<SrealityHttpClient>()
+                        .AddSingleton<BezrealitkyMapper>()
+                        .AddSingleton<SrealityMapper>();
+            });
 
     private static Task LogAsync(LogMessage log)
     {
