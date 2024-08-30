d0425bdfcc7d26b49dff4414dbbe78c67e736f94
jacob <formanekjakub12@gmail.com>
2024-08-30 11:41:35 +0200

    feat: redesign and make discord interaction work

--- Modules/RegisterModule.cs
+++ Modules/RegisterModule.cs
@@ -1,51 +1,130 @@
+using System.Text;
 using Discord;
 using Discord.Interactions;
+using HouseScout.Filters;
+using HouseScout.Model;
 
-namespace HouseScout.Modules;
-
-public class RegisterModule : InteractionModuleBase<SocketInteractionContext>
+namespace HouseScout.Modules
 {
-    // Registers a command that will respond with a modal.
-    [SlashCommand("register", "Register your preferences")]
-    public async Task Command()
-        => await Context.Interaction.RespondWithModalAsync<EstateModal>("register_modal");
-
-    // Defines the modal that will be sent.
-    public class EstateModal : IModal
+    public class RegisterModule : InteractionModuleBase<SocketInteractionContext>
     {
-        public string Title => "Estate Details";
-
-        [InputLabel("Price")]
-        [ModalTextInput("estate_price", placeholder: "500000", maxLength: 20)]
-        public double Price { get; set; }
+        private readonly DataFilter _filter;
+        private static int MinPrice { get; set; }
+        private static int MaxPrice { get; set; }
+        private static int MinSurface { get; set; }
+        private static int MaxSurface { get; set; }
+        private static OfferType OfferType { get; set; }
+        private static EstateType EstateType { get; set; }
+        public RegisterModule(DataFilter filter)
+        {
+            _filter = filter;
+        }
         
-        [InputLabel("Surface Area ( m ^ 2 )")]
-        [ModalTextInput("estate_surface", placeholder: "60", maxLength: 20)]
+
+        public class EstateModal : IModal
+        {
+            public string Title => "Estate Details";
+            
+            [InputLabel("Min Price")]
+            [ModalTextInput("min_price", placeholder: "0", maxLength: 20)]
+            public string MinPriceInput { get; set; }
+
+            [InputLabel("Max Price")]
+            [ModalTextInput("max_price", placeholder: "99999999", maxLength: 20)]
+            public string MaxPriceInput { get; set; }
+
+            [InputLabel("Min Surface Area (m ^ 2)")]
+            [ModalTextInput("min_surface", placeholder: "0", maxLength: 20)]
+            public string MinSurfaceInput { get; set; }
+
+            [InputLabel("Max Surface Area (m ^ 2)")]
+            [ModalTextInput("max_surface", placeholder: "999", maxLength: 20)]
+            public string MaxSurfaceInput { get; set; }
+        }
         
-        public int Surface { get; set; }
-        
-
-        [InputLabel("Estate Type (1=House, 2=Apartment, etc.)")]
-        [ModalTextInput("estate_type", placeholder: "1", maxLength: 2)]
-        public int EstateType { get; set; }
-
-        [InputLabel("Offer Type (1=Sale, 2=Rent, etc.)")]
-        [ModalTextInput("offer_type", placeholder: "1", maxLength: 2)]
-        public int OfferType { get; set; }
-    }
-
+        [SlashCommand("register", "Register your preferences")]
+        public async Task Command()
+        {
+            await Context.Interaction.RespondWithModalAsync<EstateModal>("registerModal");
+        }
     
-    [ModalInteraction("register_modal")]
-    public async Task ModalResponse(EstateModal modal)
-    {
-        
-        string message = $"New estate listing:\n" +
-                                $"- Price: ${modal.Price}\n" +
-                                $"- Surface Area: ${modal.Surface}\n" +
-                         $"- Estate Type: {modal.EstateType}\n" +
-                         $"- Offer Type: {modal.OfferType}";
+        [ModalInteraction("registerModal")]
+        public async Task HandleEstateDetailsModalSubmit(EstateModal modal)
+        {
+            MinPrice = int.Parse(modal.MinPriceInput);
+            MaxPrice = int.Parse(modal.MaxPriceInput);
+            MinSurface = int.Parse(modal.MinSurfaceInput);
+            MaxSurface = int.Parse(modal.MaxSurfaceInput);
+            
+            
 
+            var estateSelectMenu = new SelectMenuBuilder()
+                .WithCustomId("estateType")
+                .WithPlaceholder("Select estate type")
+                .AddOption("House", "house")
+                .AddOption("Apartment", "app");
+
+            var builder = new ComponentBuilder()
+                .WithSelectMenu(estateSelectMenu);
+            
+            Console.WriteLine(MinPrice);
+            
+
+            await RespondAsync("Please select a estate type:", components: builder.Build());
+        }
         
-        await RespondAsync(message, ephemeral: true);
+        [ComponentInteraction("estateType")]
+        public async Task HandleEstateTypeSelect(string[] selectedValues)
+        {
+            Console.WriteLine(MinPrice);
+            EstateType = selectedValues[0] == "house" ? EstateType.HOUSE : EstateType.APARTMENT;
+
+            var offerSelectMenu = new SelectMenuBuilder()
+                .WithCustomId("offerType")
+                .WithPlaceholder("Select offer type")
+                .AddOption("Sale", "sale")
+                .AddOption("Rent", "rent");
+
+            var builder = new ComponentBuilder()
+                .WithSelectMenu(offerSelectMenu);
+
+            await RespondAsync("Please select a offer type:", components: builder.Build());
+        }
+
+        [ComponentInteraction("offerType" )]
+            public async Task HandleOfferTypeSelect(string[] selectedValues)
+            {
+                Console.WriteLine(MinPrice);
+                OfferType = selectedValues[0] == "sale" ? OfferType.SALE : OfferType.RENT;
+                var estates = _filter.SurfacePriceFilter( MinPrice, MaxPrice, MinSurface, MaxSurface );
+                
+                if (!estates.Any())
+                {
+                    await RespondAsync("No matching estates found.");
+                    return;
+                }
+
+                var messageBuilder = new StringBuilder();
+                foreach (var estate in estates)
+                {
+                    var estateInfo =  $"**Link:** {estate.Link}\n \n";
+
+                    if (messageBuilder.Length + estateInfo.Length > 2000)
+                    {
+                        await Context.Channel.SendMessageAsync(messageBuilder.ToString());
+                        messageBuilder.Clear();
+                    }
+
+                    messageBuilder.Append(estateInfo);
+                }
+                
+                if (messageBuilder.Length > 0)
+                {
+                    await Context.Channel.SendMessageAsync(messageBuilder.ToString());
+                }
+
+                await RespondAsync();
+            }
+        
     }
-}   
+}
--- Program.cs
+++ Program.cs
@@ -5,15 +5,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System.Reflection;
+using Discord.Commands;
 using HouseScout.Clients;
+using HouseScout.Filters;
 using HouseScout.Mappers;
 using HouseScout.Model;
+using HouseScout.Seeding;
 using Microsoft.EntityFrameworkCore;
 
 class Program
 {
     private static DiscordSocketClient _client;
     private static InteractionService _interactionService;
+    private static CommandService _commandService; 
 
     static async Task Main(string[] args)
     {
@@ -23,13 +27,16 @@ class Program
         // Configure Discord client and interaction service
         _client = host.Services.GetRequiredService<DiscordSocketClient>();
         _interactionService = host.Services.GetRequiredService<InteractionService>();
+        _commandService = host.Services.GetRequiredService<CommandService>();
 
         _client.Log += LogAsync;
         _interactionService.Log += LogAsync;
+        _commandService.Log += LogAsync;
+        
 
         // Register the interaction handler                     
         _client.InteractionCreated += async interaction =>
-        {
+        {   
             var context = new SocketInteractionContext(_client, interaction);
             await _interactionService.ExecuteCommandAsync(context, host.Services);
         };
@@ -47,31 +54,33 @@ class Program
             await _interactionService.RegisterCommandsGloballyAsync();
         };
         
+        
+        
         // Main scope of application
         using (var scope = host.Services.CreateScope())
         {
-            //TODO this is just temp, delete later
-            var services = scope.ServiceProvider;
-            var dbContext = services.GetRequiredService<HouseScoutContext>();
-
-            var bezrealitkyClient = services.GetRequiredService<BezrealitkyGraphQLClient>();
-            var srealityClient = services.GetRequiredService<SrealityHttpClient>();
-
-            var bezrealitkyMapper = services.GetRequiredService<BezrealitkyMapper>();
-            var srealityMapper = services.GetRequiredService<SrealityMapper>();
-
-            var bezrealitkyResponse = await bezrealitkyClient.GetAdvertsAsync();
-            var srealityResponse = await srealityClient.GetSrealityData();
-
-            var bezrealitkyMappedData = bezrealitkyMapper.MapResponseToModel(bezrealitkyResponse);
-            var srealityMappedData = srealityMapper.MapResponseToModel(srealityResponse);
-
-            await dbContext.Estates.AddRangeAsync(bezrealitkyMappedData);
-            await dbContext.Estates.AddRangeAsync(srealityMappedData);
-
-            await dbContext.SaveChangesAsync();
+                /*var services = scope.ServiceProvider;
+                var dbContext = services.GetRequiredService<HouseScoutContext>();
+                
+                var bezrealitkyClient = services.GetRequiredService<BezrealitkyGraphQLClient>();
+                var srealityClient = services.GetRequiredService<SrealityHttpClient>();
+                
+                var bezrealitkyMapper = services.GetRequiredService<BezrealitkyMapper>();
+                var srealityMapper = services.GetRequiredService<SrealityMapper>();
+                
+                var bezrealitkyResponse = await bezrealitkyClient.GetAdvertsAsync();
+                var srealityResponse = await srealityClient.GetSrealityData();
+                
+                var bezrealitkyMappedData = bezrealitkyMapper.MapResponseToModel(bezrealitkyResponse);
+                var srealityMappedData = srealityMapper.MapResponseToModel(srealityResponse);
+                
+                await dbContext.Estates.AddRangeAsync(bezrealitkyMappedData);
+                await dbContext.Estates.AddRangeAsync(srealityMappedData);
+                
+                await dbContext.SaveChangesAsync(); */  
+            
         }
-
+        
         await Task.Delay(-1); // Keep the bot running
     }
 
@@ -82,22 +91,33 @@ class Program
                 var configuration = context.Configuration;
 
                 services.AddSingleton(configuration)
-                        .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
-                        {
-                            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
-                        }))
-                        .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-                        .AddDbContext<HouseScoutContext>(options =>
-                            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")))
-                        .AddSingleton<BezrealitkyGraphQLClient>()
-                        .AddSingleton<SrealityHttpClient>()
-                        .AddSingleton<BezrealitkyMapper>()
-                        .AddSingleton<SrealityMapper>();
+                    .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                    {
+                        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+                    }))
+                    .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                    .AddDbContext<HouseScoutContext>(options =>
+                        options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")))
+                    .AddSingleton<BezrealitkyGraphQLClient>()
+                    .AddSingleton<SrealityHttpClient>()
+                    .AddSingleton<BezrealitkyMapper>()
+                    .AddSingleton<SrealityMapper>()
+                    .AddScoped<DbSeeder>()
+                    .AddSingleton<CommandService>()     
+                    .AddScoped<DataFilter>();
             });
 
-    private static Task LogAsync(LogMessage log)
+    private static Task LogAsync(LogMessage message)
     {
-        Console.WriteLine(log.ToString());
+        if (message.Exception is CommandException cmdException)
+        {
+            Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
+                              + $" failed to execute in {cmdException.Context.Channel}.");
+            Console.WriteLine(cmdException);
+        }
+        else 
+            Console.WriteLine($"[General/{message.Severity}] {message}");
+
         return Task.CompletedTask;
     }
 }
