86ed9d3f10a5bd1dfe6b8e23ee37d9bef46b1841
jacob <formanekjakub12@gmail.com>
2024-08-30 11:41:13 +0200

    feat: add filter

---
+++ Filters/DataFilter.cs
@@ -0,0 +1,22 @@
+using HouseScout.Model;
+
+namespace HouseScout.Filters;
+
+public class DataFilter
+{
+    private HouseScoutContext _context;
+    
+    public DataFilter(HouseScoutContext context)
+    {
+        _context = context;
+    }
+
+    public List<Estate> SurfacePriceFilter(int priceMin, int priceMax, int surfaceMin, int surfaceMax)
+    {
+        return _context.Estates.Where(e => e.Price >= priceMin && e.Price <= priceMax && 
+                                           e.Surface >= surfaceMin && e.Surface <= surfaceMax).ToList();
+    }
+
+    
+    
+}
