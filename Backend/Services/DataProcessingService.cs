f80c2d44176ed27e8738333ad90d951a04c296d6
Matúš Formánek <formanekmato@gmail.com>
2024-09-02 09:47:23 +0200

    formatting

--- Backend/Services/DataProcessingService.cs
+++ Backend/Services/DataProcessingService.cs
@@ -23,41 +23,38 @@ public class DataProcessingService
     public async Task ProcessData()
     {
         var existingEstates = _context.Estates.ToList();
-        
+
         var fetchedEstates = new List<Estate>();
-        
+
         foreach (var kvp in _clientsAndMappers)
         {
             IClient client = kvp.Key;
             IMapper mapper = kvp.Value;
-            
 
             var data = await client.FetchDataAsync();
             fetchedEstates.AddRange(mapper.MapResponseToModel(data));
         }
-        
+
         var fetchedEstatesSet = new HashSet<string>(fetchedEstates.Select(fe => fe.ApiId));
         var existingEstatesSet = new HashSet<string>(existingEstates.Select(fe => fe.ApiId));
-        
-        
+
         var estatesToAdd = fetchedEstates
             .Where(fe => !existingEstatesSet.Contains(fe.ApiId))
             .ToList();
-        
+
         var estatesToRemove = existingEstates
             .Where(ee => !fetchedEstatesSet.Contains(ee.ApiId))
             .ToList();
-        
+
         if (estatesToAdd.Any())
         {
             await _context.Estates.AddRangeAsync(estatesToAdd);
         }
-        
+
         if (estatesToRemove.Any())
         {
             _context.Estates.RemoveRange(estatesToRemove);
         }
         await _context.SaveChangesAsync();
     }
-    
 }
