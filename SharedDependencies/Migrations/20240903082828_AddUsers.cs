ce2c11229909023dcd02ef3d62163826d22c7375
jacob <formanekjakub12@gmail.com>
2024-09-03 10:33:49 +0200

    feat: migrations

---
+++ SharedDependencies/Migrations/20240903082828_AddUsers.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SharedDependencies.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MinPrice = table.Column<int>(type: "integer", nullable: false),
+                    MaxPrice = table.Column<int>(type: "integer", nullable: false),
+                    MinSurface = table.Column<int>(type: "integer", nullable: false),
+                    MaxSurface = table.Column<int>(type: "integer", nullable: false),
+                    EstateType = table.Column<int>(type: "integer", nullable: false),
+                    OfferType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
