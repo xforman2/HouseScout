7662e744da99d68afdddc5c4026aecf3a88827e9
jacob <formanekjakub12@gmail.com>
2024-09-03 10:33:04 +0200

    feat: add User table

---
+++ SharedDependencies/Model/User.cs
@@ -0,0 +1,22 @@
+using HouseScout.Model;
+
+namespace SharedDependencies.Model;
+
+public class User
+{
+    public int UserId { get; set; }
+
+    public int MinPrice { get; set; }
+    
+    public int MaxPrice { get; set; }
+
+    public int MinSurface { get; set; }
+    
+    public int MaxSurface { get; set; }
+    
+    public EstateType EstateType { get; set; }
+    
+    public OfferType OfferType { get; set; }
+    
+
+}
